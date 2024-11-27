3043c29824fb2cf8a52fa3dceb1897ec9f8bf8cb
Kruno79 <kpopic@gmail.com>
2024-11-27 19:20:20 +0100

    postavljanje naredbi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,7 +1,15 @@
-﻿--create database edunovawp7;
+﻿use master;
+go
+drop database if exists edunovawp7;
+go
 
---use edunovawp7;
 
+
+
+create database edunovawp7;
+go
+use edunovawp7;
+go
 create table smjer(
 naziv varchar(50)
 );
