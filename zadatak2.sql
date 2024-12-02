86dfee788b500d1a032bab933f1cb50324d6bcab
Kruno79 <kpopic@gmail.com>
2024-12-02 19:57:39 +0100

    Update zadatak2.sql

--- zadatak2.sql
+++ zadatak2.sql
@@ -8,8 +8,18 @@ use skola;
 go
 
 create table ispitnirok(
-sifra int,
-predmet varchar(50),
-vrstaispita varchar(50),
-datum datetime
+sifra int not null primary key identity(1,1),
+predmet varchar(50) not null,
+vrstaispita varchar(50) not null,
+datum datetime,
+pristupio bit  not null
+);
+
+create table pristupnici(
+sifra int not null primary key identity(1,1),
+ispitnirok varchar(50) not null,
+student varchar(50) not null,
+brojbodova int not null,
+ocjena int not null
+
 );
