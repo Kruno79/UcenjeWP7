528d13db4d3d961c5cf1d7722655facf7870232a
Kruno79 <kpopic@gmail.com>
2024-12-09 20:14:11 +0100

    Update2

--- zadaca_tablica_prodaja_automobila.sql
+++ zadaca_tablica_prodaja_automobila.sql
@@ -9,9 +9,9 @@ go
 
 CREATE TABLE automobili (
   sifra int not null primary key identity(1,1),
-  marka VARCHAR(50),
-  model VARCHAR(50),
-  motor VARCHAR(50),
+  marka VARCHAR(50) not null,
+  model VARCHAR(50) not null,
+  motor VARCHAR(50) not null,
   vin VARCHAR(17),
   godiste INT,
   kilometraza INT
@@ -19,25 +19,25 @@ CREATE TABLE automobili (
 
 CREATE TABLE otkupi (
  sifra int not null primary key identity(1,1),
-  automobil INT,
-  cijena DECIMAL(10,2),
-  datum DATE,
-  prodavatelj VARCHAR(100),
+  automobil INT not null,
+  cijena DECIMAL(10,2) not null,
+  datum DATE not null,
+  prodavatelj VARCHAR(100) not null,
   FOREIGN KEY (automobil) REFERENCES automobili(sifra)
 );
 
 CREATE TABLE dorade (
   sifra int not null primary key identity(1,1),
-  otkup INT,
-  cijena_dijelova DECIMAL(10,2),
-  cijena_rada DECIMAL(10,2),
+  otkup INT not null,
+  cijena_dijelova DECIMAL(10,2) not null,
+  cijena_rada DECIMAL(10,2) not null,
   FOREIGN KEY (otkup) REFERENCES otkupi(sifra)
 );
 
 CREATE TABLE prodaja (
   sifra int not null primary key identity(1,1),
-  dorada INT,
-  cijena DECIMAL(10,2),
+  dorada INT not null,
+  cijena DECIMAL(10,2) not null,
   FOREIGN KEY (dorada) REFERENCES dorade(sifra)
 );
 
@@ -46,11 +46,11 @@ insert into automobili (marka,model,motor,vin,godiste,kilometraza) values
 ('Renault','Scenic','15dci','VF1JZ0A0643508580',2010,223017);
 
 insert into otkupi(automobil,cijena,datum,prodavatelj) values
-(1,'3000.00','2024-12-09','ivan_horvat');
+(1,3000,'2024-12-09','Ivan Horvat');
 
 insert into dorade (otkup,cijena_dijelova,cijena_rada) values
 (1, '500.00', '300.00');
 
 insert into prodaja (dorada,cijena) VALUES 
-(1, '4300.00');
+(1, 4300);
 
