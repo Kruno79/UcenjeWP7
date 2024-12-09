10041daa0f447ad9558ec2cff3ffa463d2487251
Kruno79 <kpopic@gmail.com>
2024-12-09 19:22:25 +0100

    Update

--- zadaca_tablica_prodaja_automobila.sql
+++ zadaca_tablica_prodaja_automobila.sql
@@ -1,14 +1,14 @@
 ﻿use master;
 go
-drop database if exists edunovawp7;
+drop database if exists prodaja;
 go
-create database edunovawp7 collate Croatian_CI_AS;
+create database prodaja collate Croatian_CI_AS;
 go
-use edunovawp7;
+use prodaja;
 go
 
 CREATE TABLE automobili (
-  sifra INT PRIMARY KEY,
+  sifra int not null primary key identity(1,1),
   marka VARCHAR(50),
   model VARCHAR(50),
   motor VARCHAR(50),
@@ -18,7 +18,7 @@ CREATE TABLE automobili (
 );
 
 CREATE TABLE otkupi (
-  sifra INT PRIMARY KEY,
+ sifra int not null primary key identity(1,1),
   automobil INT,
   cijena DECIMAL(10,2),
   datum DATE,
@@ -27,7 +27,7 @@ CREATE TABLE otkupi (
 );
 
 CREATE TABLE dorade (
-  sifra INT PRIMARY KEY,
+  sifra int not null primary key identity(1,1),
   otkup INT,
   cijena_dijelova DECIMAL(10,2),
   cijena_rada DECIMAL(10,2),
@@ -35,7 +35,7 @@ CREATE TABLE dorade (
 );
 
 CREATE TABLE prodaja (
-  sifra INT PRIMARY KEY,
+  sifra int not null primary key identity(1,1),
   dorada INT,
   cijena DECIMAL(10,2),
   FOREIGN KEY (dorada) REFERENCES dorade(sifra)
@@ -43,14 +43,14 @@ CREATE TABLE prodaja (
 
 
 insert into automobili (marka,model,motor,vin,godiste,kilometraza) values
-('Renault','Scenic','15dci','VF1JZ0A0643508580','2010','223017');
+('Renault','Scenic','15dci','VF1JZ0A0643508580',2010,223017);
 
 insert into otkupi(automobil,cijena,datum,prodavatelj) values
-('2','3000.00','2024-12-09','ivan_horvat')
+(1,'3000.00','2024-12-09','ivan_horvat');
 
 insert into dorade (otkup,cijena_dijelova,cijena_rada) values
-('1', '500.00', '300.00');
+(1, '500.00', '300.00');
 
 insert into prodaja (dorada,cijena) VALUES 
-('1', '4300.00');
+(1, '4300.00');
 
