cb132c7faf3dbe43597b6d314f3ad5e47203ba1f
Kruno79 <kpopic@gmail.com>
2024-12-18 20:54:53 +0100

    Početak

---
+++ CSHARP/Ucenje/E01Ulazizlaz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01Ulazizlaz
+    {
+
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("Pozdrav svijetu");
+
+
+
+            Console.Write("Ovo je u liniji");
+            Console.Write("ovo je u istoj liniji ali nakon ispisa ode u novu liniju");
+            Console.WriteLine("1. red\n2. red\tnakon taba");
+
+            Console.WriteLine(1);
+            Console.WriteLine(true);
+            Console.WriteLine(4.6);
+
+
+            // Ulaz
+            int i;
+            Console.WriteLine("Unesi cijeli broj: ");
+            i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unio si {0}", i);
+
+            //Ulaz
+
+            Console.WriteLine("Unesi ime grada: ");
+            string grad = Console.ReadLine();
+
+            //izlaz
+            Console.WriteLine("Unio si " + grad);
+
+
+         
+
+        }
+
+
+
+
+    }
+}
---
+++ CSHARP/Ucenje/E03Operatori.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E03Operatori
+    {
+
+
+        public static void Izvedi()
+        {
+            int i, j;
+
+            i=2; j=3;
+
+            i += j;
+
+            Console.WriteLine(i/j);
+            Console.WriteLine(i/(float)j);
+
+            Console.WriteLine( 5 % 2 );
+
+
+            // increment i decrement
+
+            i = 1;
+            i = i + 1;
+            i += 1;
+            i++;
+            ++i;
+
+
+            Console.WriteLine("***********");
+            i = 1;
+            Console.WriteLine(i);
+            Console.WriteLine(i++);
+            Console.WriteLine(++i);
+
+            int x = 1, y = 0;
+            x = x  --y;
+            Console.WriteLine(++x - y--);
+
+
+
+        } 
+
+
+
+    }
+}
